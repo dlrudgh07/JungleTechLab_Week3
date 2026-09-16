41b62f579694118494afa4100c3d9bccb0c1df1e
조혁 <56536360+HyeokJo@users.noreply.github.com>
2026-09-16 23:58:04 +0900

    [Fix] 머티리얼 틴트 초기값 되돌리기

--- JungleTechLab_Week3/Material.h
+++ JungleTechLab_Week3/Material.h
@@ -14,7 +14,7 @@ public:
 
 	class UTexture* BaseTexture = nullptr;
 
-	FVector4 TintColor = FVector4(0.0f, 0.0f, 0.0f, 1.0f);
+	FVector4 TintColor = FVector4(0.0f, 0.0f, 0.0f, 0.0f);
 
 	UMaterial() = default;
 	~UMaterial() = default;
